a5d4195fe9255dea4e05968aa9598ea92ccb9c44
m32889977 <m32889977@gmail.com>
2025-09-25 18:03:40 +0200

    finished lab01

--- lab01/1.bash
+++ lab01/1.bash
@@ -1,3 +1,15 @@
 docker pull alpine # pull a new image
 docker images # see all images on our system
+
+# Testing some runs with different output
 docker run alpine ls -l
+docker run alpine echo "hello from alpine"
+
+# Shell will exit instantly
+docker run alpine /bin/sh
+
+# Same thing but with an interactive shell
+docker run -it alpine /bin/sh
+
+docker ps # shows all running containers
+docker ps -a # shows previously run containers
